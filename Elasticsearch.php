5302bdd2d8eadfe7d69ea88ec45ee4e8192362e3
dmitriy_kulinich <kulinich.dmitriy@gmail.com>
2015-07-09 14:56:35 +0600

    fixs

--- Elasticsearch.php
+++ Elasticsearch.php
@@ -11,40 +11,47 @@ class Elasticsearch extends Component
     public $index;
     public $type;
 
-    public static function createDocument($node_address, $index, $type, $data, $id = null, $options = [])
+    public static function createDocument($node_address, $index, $type, $data, $id, $options = [])
     {
         if (empty($data)) {
             $body = '{}';
         } else {
             $body = is_array($data) ? Json::encode($data) : $data;
         }
-        if ($id !== null) {
-            $query = curl_init('http://' . $node_address . '/' . $index . '/' . $type . '/' . $id . '?' . $options);
-            curl_setopt($query, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($query, CURLOPT_POSTFIELDS, $body);
-            curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($query, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json',
-                    'Content-Length: ' . strlen($body))
-            );
+        $query = curl_init('http://' . $node_address . '/' . $index . '/' . $type . '/' . $id . '?' . $options);
+        curl_setopt($query, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($query, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($query, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($body))
+        );
 
-            $result = curl_exec($query);
+        $result = curl_exec($query);
 
-            return $result;
+        return $result;
 
+    }
+
+    public static function updateDocument($node_address, $index, $type, $data, $id, $options = [])
+    {
+        if (empty($data)) {
+            $body = '{}';
         } else {
-            $query = curl_init('http://' . $node_address . '/' . $index . '/' . $type . '/' . $id . '?' . $options);
-            curl_setopt($query, CURLOPT_CUSTOMREQUEST, "PUT");
-            curl_setopt($query, CURLOPT_POSTFIELDS, $body);
-            curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($query, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json',
-                    'Content-Length: ' . strlen($body))
-            );
-
-            $result = curl_exec($query);
-
-            return $result;
+            $body = is_array($data) ? Json::encode($data) : $data;
         }
+
+        $query = curl_init('http://' . $node_address . '/' . $index . '/' . $type . '/' . $id . '?' . $options);
+        curl_setopt($query, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($query, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($query, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($body))
+        );
+
+        $result = curl_exec($query);
+
+        return $result;
     }
 }
